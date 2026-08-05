93fcd3963691500a67ccb2ab26b4c2d2cdd958e1
liunian_ximu <1323871132@qq.com>
2026-08-06 01:17:54 +0800

    移除PWA相关功能及依赖

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
 
-declare module 'virtual:pwa-register/react' {
-  export function useRegisterSW(options?: any): {
-    needRefresh: readonly [boolean, (v: boolean) => void]
-    offlineReady: readonly [boolean, (v: boolean) => void]
-    updateServiceWorker: (reloadPage?: boolean) => Promise<void>
-  }
-}
